db04f7b28e8e715567f82ae78711fb2637d18c5e
“dziekan30” <dziekan30@gmail.com>
2020-02-14 13:10:45 -0600

    new

---
+++ db/seeds.rb
@@ -0,0 +1,17 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+# post = Post.new(title: "Dziady", body: "Welcome to my word")
+# post.save
+
+# post = Post.new(title: "Calm down", body: "Josh is here so sh......")
+# post.save
+
+# post = Post.new(title: "Day3", body: "lalalalalalalalalalalalalalala")
+# post.save
---
+++ public/index.js
@@ -0,0 +1,6 @@
+axios
+    .get("http://localhost:3000/api/posts")
+    .then(function(response) {
+    var posts = response.data;
+    console.log(posts);
+    });
